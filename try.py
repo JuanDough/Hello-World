5745cfb7fd72e15313e3f6fa55f906dc3d4760a4
Juan Dough-Nut <daniel.oneill@parsons.com>
2016-12-29 18:40:10 -0500

    Add files via upload

---
+++ try.py
@@ -0,0 +1,11 @@
+day=32
+
+try :
+    if day > 31 :
+        raise ValueError as ('invalid day number')
+            except ValueError as msg:
+            print('the program found an', msg)
+
+            finally:
+                print('but saul good')
+                
